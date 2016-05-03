88975ca687d9da4916992fbebaf0252e52cbe59d
Federico Squartini <federico.squartini@gmail.com>
2016-05-03 15:50:38 +0200

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,7 @@
+import { Template } from 'meteor/templating'
+import { ReactiveVar } from 'meteor/reactive-var';
+import 'lightbox2'
+import 'lightbox2/dist/css/lightbox'
+//import 'lightbox2/dist/js/lightbox'
+import './main.html';
+
